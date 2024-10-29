176668c3440f9b1a07dfefbc9adf2d17736970b9
daniel lance <poorfoolspencil@gmail.com>
2024-10-28 20:14:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Append new ID to the DOM
+            const idElement = document.createElement("p");
+            idElement.textContent = `ID: ${data.id}`;
+            document.body.appendChild(idElement);
+        })
+        .catch(error => {
+            // Append error message to the DOM
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = `Error: ${error.message}`;
+            document.body.appendChild(errorMessage);
+        });
+}
